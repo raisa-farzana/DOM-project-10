6ea8955d058c2915230e4d2e8961078fb5508df1
raisa-farzana <raisafarzana5502@gmail.com>
2025-04-08 20:30:51 +0600

    updated html css js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const calculateBMI = () => {
+    const age = document.getElementById('age').value;
+    const height = parseFloat(document.getElementById('height').value/100);
+    const weight = parseFloat(document.getElementById('weight').value);
+
+}
